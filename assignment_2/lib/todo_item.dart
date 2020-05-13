a164c44296fe166b7088716780860467fbd7f163
waqas.karim <waqas.karim@venturedive.com>
2020-05-13 09:32:37 +0500

    whatsapp ui assignment

---
+++ assignment_2/lib/todo_item.dart
@@ -0,0 +1,5 @@
+class TodoItem {
+  String title;
+
+  TodoItem({this.title});
+}
---
+++ whatsapp_ui/lib/main.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+        // This makes the visual density adapt to the platform that you run
+        // the app on. For desktop platforms, the controls will be smaller and
+        // closer together (more dense) than on mobile platforms.
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  HomePage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  TabController _tabController;
+
+  final appBar = AppBar(
+    title: Text("Whatsapp UI"),
+    bottom: TabBar(
+      tabs: <Widget>[
+        Tab(text: "CHATS"),
+        Tab(text: "STATUS"),
+        Tab(text: "CALLS")
+      ],
+    ),
+    actions: <Widget>[
+      Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Icon(Icons.search),
+      ),
+      Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Icon(Icons.more_vert),
+      ),
+    ],
+  );
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 3,
+      initialIndex: 1,
+      child: Scaffold(
+        appBar: appBar,
+        body: TabBarView(
+            controller: _tabController,
+            children: <Widget>[ChatWidgets(), StatusWidgets(), CallsWidgets()]),
+      ),
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(vsync: null, length: 3);
+  }
+}
+
+class ChatWidgets extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Text("No chats");
+  }
+}
+
+class StatusWidgets extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Text("No Status yet.");
+  }
+}
+
+class CallsWidgets extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Text("No call logs");
+  }
+}
